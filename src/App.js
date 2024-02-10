1e6a8a6a394c441827698b870e6e7bbbf59579ed
NEW-BORN <manavbhullar341@gmail.com>
2024-02-10 15:16:05 +0530

    Refactor Square component and add Board component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, { useState } from 'react';
+
+
+function Square({ value }) {
+  return (
+    <button className="square" onClick="{onSquareClick}">
+      {value}
+    </button>
+  );
+}
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice(i);
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
+    </>
+  );
 }
